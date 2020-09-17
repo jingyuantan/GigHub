f76913005ee7322a2b3237ce8db9b8d1c956e956
jingyuantan <jingyuantan1028@gmail.com>
2020-09-17 12:41:06 +0100

    Created add a gig form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // added to read data from database and display at view
+        private readonly ApplicationDbContext _context;
+        // initialize _context
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            // create a model to pass to view page for dropdown list
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
