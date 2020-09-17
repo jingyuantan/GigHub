30ab7f7404a7e9e4406f2509be5585b8597b992d
jingyuantan <jingyuantan1028@gmail.com>
2020-09-17 10:31:04 +0100

    created the gig model

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {   
+        // remember to add reference here after adding any new model
+        // reason to add here is just to let entity framework find the class
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        //who, when, where, what genre
+        //who - already declared by default in identity model, we call it
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        // we do not want to duplicate genre in database, so we create seperate class to store it
+    }
+}
